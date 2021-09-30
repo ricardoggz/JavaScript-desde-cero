a98bcf94508e47943c6462e097f37c0637f7bc14
ricardoggz <ricardo_ggzicm@outlook.com>
2021-09-30 12:18:32 -0500

    Operadores

--- variables/operadores.js
+++ variables/operadores.js
@@ -9,25 +9,38 @@ console.log(potencia);
 
 var comparacion = 3;
 var comparacion2 = 2;
+console.log(comparacion==comparacion2); //El operador == imprimirá "true" si las variables son del mismo valor, en caso contrario imprimirá "false"
 
-var comparar = comparacion == comparacion2; //El operador == imprimirá "true" si las variables son del mismo valor, en caso contrario imprimirá "false"
-console.log(comparar);
+//
+
+var verdadero = 2;
+var verdadero2 ='2';
+console.log(verdadero===verdadero2) //El operador === imprimirá true si las variables tienen el mismo valor y el mismo tipo
+
+//
 
 var comparacion3 = 2;
 var comparacion4 = 1;
-var comparar2 = comparacion3 !== comparacion4; //El operador !== imprimira "true" si ambas variables no son del mismo valor, en caso contrario imprimirá "false"
-console.log(comparar2);
+console.log(comparacion3!=comparacion4); //El operador != imprimira "true" si ambas variables no son del mismo valor, en caso contrario imprimirá "false"
+
+//
 var valor2 = 1;
 var valor3 = 1;
-var compararValores = valor2 !== valor3;//El operador !== imprimira "true" si ambas variables no son del mismo valor, en caso contrario imprimirá "false"
-console.log(compararValores);
+console.log(valor2!=valor3); //El operador != imprimira "true" si ambas variables no son del mismo valor, en caso contrario imprimirá "false"
+
+//
+
+var mivalor = 2;
+var mivalor2 = '2';
+console.log(mivalor!==mivalor2);// El operador !== imprimirá "true" si ambas variables no son del mismo valor y/o  del mismo tipo de variable
+
+//
 
 var valor4 = 1;
 var valor5 = 0.5;
+console.log(valor4 > valor5);//El operqador > imprimirá "true" si la variable antes de ">" es mayor a la variable despues de ">", de lo contrario imprimirá "false"
 
-var compararValores2 = valor4 > valor5; //El operqador > imprimirá "true" si la variable antes de ">" es mayor a la variable despues de ">", de lo contrario imprimirá "false"
-console.log(compararValores2);
-var compararValores3 = valor4 < valor5; //El operdor < imprimirá "true" si la variable antes de "<" es menor a la variable despues de "<", de lo contrario imprimirá "false"
-console.log(compararValores3);
+
+console.log(valor4 < valor5)//El operdor < imprimirá "true" si la variable antes de "<" es menor a la variable despues de "<", de lo contrario imprimirá "false"
 
 
