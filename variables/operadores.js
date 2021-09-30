55cea1745b5e1ac72551b2dd59e01cf24d1ff285
ricardoggz <ricardo_ggzicm@outlook.com>
2021-09-29 23:27:18 -0500

    Nuevos archivos

---
+++ variables/operadores.js
@@ -0,0 +1,33 @@
+var valor = 2; //Operadores de asignacion (=, +=, /=, **=, *=, %=)
+valor = valor + 3; //operador aritmético (+ - * / % **)
+console.log(valor);
+
+var potencia = 3 ** 2; //operador aritmetico exponencial
+console.log(potencia);
+
+//operadores de comparación
+
+var comparacion = 3;
+var comparacion2 = 2;
+
+var comparar = comparacion == comparacion2; //El operador == imprimirá "true" si las variables son del mismo valor, en caso contrario imprimirá "false"
+console.log(comparar);
+
+var comparacion3 = 2;
+var comparacion4 = 1;
+var comparar2 = comparacion3 !== comparacion4; //El operador !== imprimira "true" si ambas variables no son del mismo valor, en caso contrario imprimirá "false"
+console.log(comparar2);
+var valor2 = 1;
+var valor3 = 1;
+var compararValores = valor2 !== valor3;//El operador !== imprimira "true" si ambas variables no son del mismo valor, en caso contrario imprimirá "false"
+console.log(compararValores);
+
+var valor4 = 1;
+var valor5 = 0.5;
+
+var compararValores2 = valor4 > valor5; //El operqador > imprimirá "true" si la variable antes de ">" es mayor a la variable despues de ">", de lo contrario imprimirá "false"
+console.log(compararValores2);
+var compararValores3 = valor4 < valor5; //El operdor < imprimirá "true" si la variable antes de "<" es menor a la variable despues de "<", de lo contrario imprimirá "false"
+console.log(compararValores3);
+
+
