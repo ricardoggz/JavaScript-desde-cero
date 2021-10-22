1f4cd2e6237628982232835e3e8780dd479163d1
ricardoggz <ricardo_ggzicm@outlook.com>
2021-10-21 19:20:51 -0500

    Integracion de mas tipos de variables

--- variables/tipos-variables.js
+++ variables/tipos-variables.js
@@ -1,20 +1,39 @@
 //tipo STRING
 var Minombre = 'Ricardo';
 console.log(typeof Minombre);
+
 //tipo booleano
 var nombre = true;
 var apellido = false;
-console.log(typeof nombre, apellido);
+console.log(typeof nombre, typeof apellido);
+
+
 //tipo number
 var edad = 21;
 console.log(typeof edad);
+
 //tipo decimal
 var porcentaje = 31.5;
 console.log(typeof porcentaje);
+
 //tipo null
 var nulo = null;
 console.log(typeof nulo);
+
 //undefined
 var indefinido = undefined;
-console.log(typeof indefinido);
-//
+console.log(typeof indefinido); 
+
+//tipo Symbol
+var symbol = Symbol();
+console.log(typeof symbol);
+
+//tipo clase
+class nombres {
+    constructor(Minombre, Miapellido){
+        this.Minombre = Minombre;
+        this.Miapellido = Miapellido;
+    }
+    
+} console.log(typeof nombres);
+
