37a8cd869ebd1ea0c994a09f43f9a558ee6b705e
ricardoggz <ricardo_ggzicm@outlook.com>
2021-10-25 16:14:24 -0500

    complementacion de conceptos

--- variables/variables.js
+++ variables/variables.js
@@ -1,11 +1,12 @@
-// var: variables globales
+// var: variables globales // ya no es recomendado usar var
 var saludo = 'Hola mundo';
+
 if (saludo) {
     var saludo = 'Hola :D';
     console.log(saludo);
 }
 console.log(saludo);
-// let: variables limitadas al alcance del bloque donde las definimos
+// let: variables limitadas al alcance del bloque donde las definimos, es decir que pueden ser mutables //let y const respetan el flujo de la programación // 
 
 let nombre = 'Ricardo'
 console.log(nombre);
@@ -13,6 +14,6 @@ if (nombre) {
     let nombre = 'Jose'
     console.log(nombre);
 }
-// const: variables inmutables
+// const: variables inmutables //Es más recomendable programar con variables inmutables
 const apellido = 'Guevara';
 console.log(apellido);
