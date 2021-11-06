b04fd4fa4a5fe7d2de38f7d859b3c33dacd2e0ba
ricardoggz <ricardo_ggzicm@outlook.com>
2021-11-05 19:52:52 -0600

    peticion con axios

--- Axios/axios.js
+++ Axios/axios.js
@@ -1,5 +1,14 @@
 //axios es una librería para hacer peticiones http
 
-axios
+/*axios
 .get('https://jsonplaceholder.typicode.com/users/1')
-.then(({data})=>console.log(data));
+.then(({data})=>console.log(data));*/
+const URL = "http://127.0.0.1:5500/Axios/peticion.html";
+
+axios.get(URL).then(function showData({ data }) {
+  document
+    .getElementById("btn-peticion")
+    .addEventListener("click", function () {
+      document.getElementById("show-peticion").innerHTML = data;
+    });
+});
